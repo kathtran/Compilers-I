b5b6dc56da4b886deab2e36dc51b517acd0c27b2
kathtran <kathleenvdt@gmail.com>
2015-12-01 18:02:51 -0800

    task 2: two tests left

--- hw4/Ast.java
+++ hw4/Ast.java
@@ -405,8 +405,6 @@ class Ast {
         }
 
         VarSet checkVarInit(VarSet initSet) throws Exception {
-            if (!initSet.contains(nm))
-                throw new StaticError("Uninitialized variable " + nm + "\n");
             obj.checkVarInit(initSet);
             for (Exp e : args)
                 e.checkVarInit(initSet);
@@ -669,8 +667,6 @@ class Ast {
         }
 
         void checkVarInit(VarSet initSet) throws Exception {
-//            if (!initSet.contains(nm))
-//                throw new StaticError("Uninitialized variable " + nm + "\n");
             initSet.add(nm);
             return;
         }
